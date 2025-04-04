b84c0d5d07337943ecb53c54e0eca838fd34ddd7
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2025-04-04 08:47:30 +0600

    removed some files

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d easy-rent-backend-c-env-1.eba-eyrfvvup.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email erkebulan.duishenaliev@gmail.com
+sudo certbot -n -d easyrent.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email erkebulan.duishenaliev@gmail.com
+# http://easyrent.eu-north-1.elasticbeanstalk.com/
