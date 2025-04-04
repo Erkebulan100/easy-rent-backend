9d53101fdbc01d1d56d5fd1f08905d5bee220819
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2025-04-04 08:59:22 +0600

    deleted

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d easyrent.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email erkebulan.duishenaliev@gmail.com
-# http://easyrent.eu-north-1.elasticbeanstalk.com/
