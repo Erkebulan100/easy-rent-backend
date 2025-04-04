aa0f4118673ad6c8dd2cd7e3f5633d0fd94d1e66
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2025-04-04 08:03:33 +0600

    Add SSL/TLS certificate configuration for Elastic Beanstalk

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Place in .platform/hooks/postdeploy directory
+sudo certbot -n -d easy-rent-backend-c-env-1.eba-eyrfvvup.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email erkebulan.duishenaliev@gmail.com
