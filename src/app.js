d0687a7340ad67555310fdbae8a99fa9a7f0c1b6
Erkebulan100 <67504870+Erkebulan100@users.noreply.github.com>
2025-04-16 10:51:03 +0600

    Fix CORS configuration to allow PATCH requests for profile updates

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ if (config.nodeEnv === 'production') {
         callback(new Error('Not allowed by CORS'));
       }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
